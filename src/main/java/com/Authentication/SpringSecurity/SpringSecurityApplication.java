da25f4bb1a7c63656db5cfdf1404642af2e13a20
JoshM128 <Joshmiller7288@gmail.com>
2023-08-28 18:00:47 -0400

    Main with example commandLineRunner method.

---
+++ src/main/java/com/Authentication/SpringSecurity/SpringSecurityApplication.java
@@ -0,0 +1,45 @@
+package com.Authentication.SpringSecurity;
+
+import com.Authentication.SpringSecurity.Service.AuthenticationService;
+import com.Authentication.SpringSecurity.Authentication.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.Authentication.SpringSecurity.User.Role.ADMIN;
+import static com.Authentication.SpringSecurity.User.Role.MANAGER;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin 1232")
+					.lastname("admin")
+					.email("admin12222@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Manager 322")
+					.lastname("manager")
+					.email("manage12332r@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};
+	}
+}
